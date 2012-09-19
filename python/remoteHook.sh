dab064a2ffd87501ceb243026a7e642cb859a340
cox <adam.cox@kit.edu>
2012-09-19 14:51:14 -0700

    final edit of README

--- python/remoteHook.sh
+++ python/remoteHook.sh
@@ -1,9 +1,7 @@
 #!/usr/local/bin/tcsh
 
 #set the version number of KDATA to use here explicitly
-set KDATA = $KDATA_ROOT
-#set KDATA = /sps/edelweis/kdata/code/v4.5
-source $KDATA/config/setup_kdata.csh #make sure environment variables are properly set
+source $KDATA_ROOT/config/setup_kdata.csh #make sure environment variables are properly set
 
-$KDATA/lib/KDataPy/scripts/dataprocess/batchRunProc1.py https://edwdbuser:***@edwdbik.fzk.de:6984 datadb /sps/edelweis/kdata/dataprocessing/schedule
-#$KDATA/lib/KDataPy/scripts/dataprocess/batchRunProc2.py https://edwdbuser:***@edwdbik.fzk.de:6984 datadb /sps/edelweis/kdata/dataprocessing/schedule
+$KDATA_ROOT/lib/KDataPy/scripts/dataprocess/batchRunProc1.py https://edwdbuser:***@edwdbik.fzk.de:6984 datadb /sps/edelweis/kdata/dataprocessing/schedule
+$KDATA_ROOT/lib/KDataPy/scripts/dataprocess/batchRunProc2.py https://edwdbuser:***@edwdbik.fzk.de:6984 datadb /sps/edelweis/kdata/dataprocessing/schedule
